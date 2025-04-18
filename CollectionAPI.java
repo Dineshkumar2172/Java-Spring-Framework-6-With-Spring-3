8ece1298c84260b406f7ab1674f707d9264c051c
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-18 18:22:18 +0530

    collection API

--- CollectionAPI.java
+++ CollectionAPI.java
@@ -3,7 +3,7 @@ import java.util.Collection;
 
 public class CollectionAPI {
     public static void main(String[] args) {
-        Collection<Integer> nums = new ArrayList<>();
+        Collection<Integer> nums = new ArrayList<Integer>();
         nums.add(10);
         nums.add(20);
         nums.add(20);
@@ -12,5 +12,13 @@ public class CollectionAPI {
         for (int num: nums) {
             System.out.println(num);
         }
+
+        // Collection nums2 = new ArrayList<>(); // always use type, by default it works with generic to support all types
+        // nums2.add(10);
+        // nums2.add(20);
+        // nums2.add(30);
+        // for (Object num: nums) {
+        //     System.out.println((Integer)num);
+        // }
     }    
 }
