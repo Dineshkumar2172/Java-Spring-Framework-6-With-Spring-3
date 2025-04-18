6d03270e01069ee2eb45e3850db0067ab13dd5b1
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-04-18 15:36:51 +0530

    functional interfaces and lambda expression

---
+++ InterfacesFunctional.java
@@ -0,0 +1,50 @@
+@FunctionalInterface // means an interface with only one method
+interface InterfaceClass {
+    void print();
+}
+
+@FunctionalInterface
+interface InterfaceClass2 {
+    void print(int n); // functional interface method which takes an input
+}
+
+class ConcreteFunctionalInterface implements InterfaceClass {
+    public void print() {
+        System.out.println("defined print function inside concrete class");
+    }
+}
+
+public class InterfacesFunctional {
+    public static void main(String[] args) {
+        InterfaceClass concreteFunctionalInterface = new ConcreteFunctionalInterface();
+        concreteFunctionalInterface.print();
+
+        // we can also make use of anonymous inner class
+        InterfaceClass interfaceClass = new InterfaceClass() {
+            @Override
+            public void print() {
+                System.out.println("defined print function inside anonymous class");
+            }
+        };
+
+        interfaceClass.print();
+
+        // we can use lambda expressions - can only be used with functional interface
+        // because for functional interface, compiler knows which method were are trying to implement
+        // since there's only one method declared in functional interfaces.
+        InterfaceClass interfaceClass2 = () -> {
+                System.out.println("defined print function inside lambda expression");
+        };
+
+        interfaceClass2.print();
+
+
+        // we can reduce the lines even further using lambda expression since we have only one line defined for interface method
+        InterfaceClass interfaceClass3 = () -> System.out.println("defined print function using lambda expression - single line");
+        interfaceClass3.print();
+
+
+        InterfaceClass2 interfaceClass22 = (int i) -> System.out.println("defined parameterised print function using lambda expression - single line" + i);
+        interfaceClass22.print(22);
+    }
+}
